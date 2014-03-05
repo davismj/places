8b8ae3612d3b8d2b77c75dd2735bf0dc992d8ffa
Matthew James Davis <davis.matthewjames@gmail.com>
2014-03-05 19:23:09 +0900

    changed app to application for elastic beanstalk support

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 import flask
  
-app = flask.Flask(__name__)
+application = flask.Flask(__name__)
  
-@app.route('/')
+@application.route('/')
 def hello_world():
     return "Hello world!"
  
 if __name__ == '__main__':
-    app.run()
+    application.run()
