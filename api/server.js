9f583d36d4131f43d08df09f36a0c124c6a2854d
Matthew James Davis <davis.matthewjames@gmail.com>
2014-10-18 14:44:35 -0400

    added cors to

--- api/server.js
+++ api/server.js
@@ -14,8 +14,19 @@ router.get('/', function(req, res) {
 
 router.use('/location', location.routes);
 router.use('/auth', auth.routes);
-
+)
 app.use(bodyParser.json({strict:false}));
 app.use(cookieParser());
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 app.use(config.path, router);
 app.listen(config.port);
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
